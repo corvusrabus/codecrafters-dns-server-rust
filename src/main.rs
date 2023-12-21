7d8e0df9314d64cecf76b2d61dbca1cd3f859bcb
Corvin Paul <corvin.paul@outlook.com>
2023-12-21 13:39:51 +0100

    questions

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@
 use std::net::UdpSocket;
 
 #[derive(Default)]
-#[repr(C)]
-pub struct DNSMessage {
+pub struct DNSMessageHeader {
     pub packet_id: u16,
     qr_oc_aa_tc_rd: u8,
     ra_z_rcode: u8,
@@ -13,22 +12,64 @@ pub struct DNSMessage {
     pub ar_count: u16,
 }
 
-impl DNSMessage {
+#[derive(Default)]
+pub struct DNSMessageQuestion {
+    names: Vec<String>,
+    r#type: u16,
+    class: u16,
+}
+
+pub struct DNSMessageBuilder {
+    header: DNSMessageHeader,
+    question: DNSMessageQuestion,
+}
+
+impl DNSMessageBuilder {
+    fn build(&self) -> Vec<u8> {
+        let mut res = Vec::new();
+        res.extend(self.header.packet_id.to_be_bytes());
+        res.push(self.header.qr_oc_aa_tc_rd);
+        res.push(self.header.ra_z_rcode);
+        res.extend(self.header.qd_count.to_be_bytes());
+        res.extend(self.header.an_count.to_be_bytes());
+        res.extend(self.header.ns_count.to_be_bytes());
+        res.extend(self.header.ar_count.to_be_bytes());
+        for name in self.question.names.iter() {
+            for label in name.split('.') {
+                res.extend((label.len() as u8).to_be_bytes());
+                res.extend(label.as_bytes())
+            }
+            res.push(0u8);
+        }
+        res.extend(self.question.r#type.to_be_bytes());
+        res.extend(self.question.class.to_be_bytes());
+        res
+    }
+    pub fn new() -> Self {
+        Self {
+            header: DNSMessageHeader { packet_id: 1234u16, ..Default::default() },
+            question: DNSMessageQuestion {
+                names: vec![],
+                r#type: 1,
+                class: 1,
+            },
+        }
+    }
+    pub fn add_name(&mut self, name: &str) {
+        self.question.names.push(name.to_string());
+        self.header.qd_count += 1;
+    }
+}
+
+impl DNSMessageBuilder {
     fn set_qr(&mut self, bit: bool) -> &mut Self {
         let bit = (bit as u8) << 7;
-        self.qr_oc_aa_tc_rd |= bit;
+        self.header.qr_oc_aa_tc_rd |= bit;
         self
     }
 }
 
 
-fn to_bytes<T>(t: &T) -> &[u8]{
-    let p: *const T = t;
-    let p = p as *const u8;
-    unsafe{
-        std::slice::from_raw_parts(p, std::mem::size_of::<T>())
-    }
-}
 // const DNS_HEADER_SIZE: usize = size_of::<DNSMessage>();
 
 fn main() {
@@ -43,12 +84,13 @@ fn main() {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
                 // debug_assert_eq!(size,DNS_HEADER_SIZE);
-                let mut header = DNSMessage { packet_id: u16::from_ne_bytes(1234u16.to_be_bytes()), ..Default::default() };
-                header.set_qr(true);
-                let bytes = to_bytes(&header);
+                let mut resp = DNSMessageBuilder::new();
+                resp.set_qr(true);
+                resp.add_name("codecrafters.io");
+                let bytes = resp.build();
                 println!("{bytes:?}");
                 udp_socket
-                    .send_to(bytes, source)
+                    .send_to(bytes.as_slice(), source)
                     .expect("Failed to send response");
                 println!("sent");
             }
